b181ae8c558b572e387134ef9bc9d5c4074f64a5
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-10-05 19:21:53 +0530

    first commit

---
+++ components/Header.js
@@ -0,0 +1,41 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
+
+
+
+const Header = () => {
+
+    const { data: session } = useSession()
+    return (
+        <header className="flex justify-between p-5 max-w-5xl mx-auto">
+            <div className="flex items-centre space-x-5">
+                <Link href="/">
+                    <img className="w-44 object-contain cursor-pointer" src=" https://links.papareact.com/yvf" alt="medium icon" />
+                </Link>
+                
+                <div className="hidden md:inline-flex items-center space-x-5">
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3 className="text-white bg-green bg-green-600 px-4 py-1 rounded-full">Follow</h3>
+                </div>
+            </div>
+
+            <div className="flex items-center space-x-5 text-green-600">
+                {session ? (
+                    <div className='flex space-x-4 '>
+                        <h3 className='text-blue-600 py-1'> Signed in as {session.user.name}</h3>
+                        <div className="border rounded-full border-red-600 px-4 py-1 cursor-pointer" onClick={() => signOut()}>Sign out</div>
+                    </div>
+                ) : (
+                    <div>
+                        <div className="border rounded-full border-green-600 px-4 py-1 cursor-pointer" onClick={() => signIn()}>Sign in</div>
+                    </div>
+                )}
+            </div>    
+        </header>
+
+        )
+  
+}
+
+export default Header
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth"
+import RedditProvider from "next-auth/providers/reddit";
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+import FacebookProvider from "next-auth/providers/facebook"
+
+
+
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET
+    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    // FacebookProvider({
+    //   clientId: process.env.FACEBOOK_ID,
+    //   clientSecret: process.env.FACEBOOK_SECRET
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET
+    // }),
+    // Passwordless / email sign in
+    // EmailProvider({
+    //   server: process.env.MAIL_SERVER,
+    //   from: 'NextAuth.js <no-reply@example.com>'
+    // }),
+  ],
+})
